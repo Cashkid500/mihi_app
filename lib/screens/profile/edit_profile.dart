ea45a84b20f586ed32fa3d2c735caf2a3334171a
BALOGUN MURTADHO <murtadhob@gmail.com>
2024-07-27 10:40:16 +0100

    Implemented the image picker functionality

--- lib/screens/profile/edit_profile.dart
+++ lib/screens/profile/edit_profile.dart
@@ -1,8 +1,11 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:mihi_app/screens/constants/asset_path.dart';
 import 'package:mihi_app/screens/constants/color_constants.dart';
 import 'package:mihi_app/screens/constants/text_constants.dart';
 import 'package:mihi_app/screens/profile/profile_settings.dart';
+import 'package:image_picker/image_picker.dart';
 
 class EditProfileScreen extends StatefulWidget {
   const EditProfileScreen({Key? key}) : super(key: key);
@@ -12,110 +15,10 @@ class EditProfileScreen extends StatefulWidget {
 }
 
 class _EditProfileScreenState extends State<EditProfileScreen> {
-  void _showBottomSheet(BuildContext context) {
-    showModalBottomSheet(
-      backgroundColor: Colors.transparent,
-      isDismissible: false,
-      context: context,
-      builder: (BuildContext context) {
-        return Container(
-          height: 200,
-          decoration: BoxDecoration(
-            color: whiteText,
-            borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(32),
-              topRight: Radius.circular(32),
-            ),
-          ),
-
-          // Content of the bottom sheet
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              ListTile(
-                title: Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(top: 30.0, left: 20.0),
-                      child: Container(
-                        height: 40,
-                        width: 40,
-                        decoration: BoxDecoration(
-                          color: Brilliant,
-                          borderRadius: BorderRadius.circular(16),
-                          image: DecorationImage(
-                              image: Image(
-                                      image:
-                                          AssetImage(MihiAppAssetsPath.upload))
-                                  .image),
-                        ),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(left: 20.0, top: 30.0),
-                      child: Text(
-                        MihiAppText.upload,
-                        style: TextStyle(
-                            color: blackText,
-                            fontWeight: FontWeight.w400,
-                            fontSize: 20),
-                      ),
-                    ),
-                  ],
-                ),
-                onTap: () {
-                  // Handle option 1
-                  // Navigator.pop(context);
-                },
-              ),
-              ListTile(
-                title: Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(top: 40.0, left: 20.0),
-                      child: Container(
-                        height: 40,
-                        width: 40,
-                        decoration: BoxDecoration(
-                          color: Brilliant,
-                          borderRadius: BorderRadius.circular(16),
-                          image: DecorationImage(
-                              image: Image(
-                                      image:
-                                          AssetImage(MihiAppAssetsPath.camera))
-                                  .image),
-                        ),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(left: 20.0, top: 40.0),
-                      child: Text(
-                        MihiAppText.photo,
-                        style: TextStyle(
-                            color: blackText,
-                            fontWeight: FontWeight.w400,
-                            fontSize: 20),
-                      ),
-                    ),
-                  ],
-                ),
-                onTap: () {
-                  // Handle option 2
-                  Navigator.pop(context);
-                },
-              ),
-            ],
-          ),
-        );
-      },
-    );
-  }
+  XFile? _selectedImage;
 
   @override
   Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
     return Scaffold(
       body: SingleChildScrollView(
         child: Column(
@@ -127,42 +30,26 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
                   padding: const EdgeInsets.only(top: 40.0, left: 10.0),
                   child: GestureDetector(
                     onTap: () {
-                      Navigator.of(context).push(MaterialPageRoute(
-                          builder: (BuildContext context) =>
-                              const ProfileSettingsScreen()));
+                      Navigator.of(context).push(
+                        MaterialPageRoute(builder: (BuildContext context) => const ProfileSettingsScreen(),),
+                      );
                     },
-                    child: Image.asset(
-                      MihiAppAssetsPath.backButton,
-                      height: 25.0,
-                    ),
+                    child: Image.asset(MihiAppAssetsPath.backButton, height: 25.0,),
                   ),
                 ),
                 Padding(
                   padding: const EdgeInsets.only(top: 40.0, left: 100.0),
-                  child: Text(
-                    MihiAppText.editProfile,
-                    style: TextStyle(
-                        fontSize: 16.0,
-                        fontWeight: FontWeight.w400,
-                        color: mithril),
-                  ),
+                  child: Text(MihiAppText.editProfile, style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.w400, color: mithril,),),
                 ),
                 Padding(
                   padding: const EdgeInsets.only(top: 40.0, left: 80.0),
-                  child: Text(
-                    MihiAppText.update,
-                    style: TextStyle(
-                        fontSize: 18.0,
-                        fontWeight: FontWeight.w500,
-                        color: spaceInvader),
-                  ),
+                  child: Text(MihiAppText.update, style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.w500, color: spaceInvader,),),
                 ),
               ],
             ),
-            const SizedBox(
-              height: 60.0,
-            ),
-
+            
+            const SizedBox( height: 60.0,),
+            
             // Profile Image
             Column(
               crossAxisAlignment: CrossAxisAlignment.start,
@@ -172,119 +59,201 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
                   children: [
                     CircleAvatar(
                       radius: 50.0,
-                      backgroundImage:
-                          AssetImage(MihiAppAssetsPath.chrisAniedi),
+                      backgroundImage: _selectedImage == null
+                          ? AssetImage(MihiAppAssetsPath.chrisAniedi)
+                          : FileImage(File(_selectedImage!.path)) as ImageProvider,
                     ),
                     GestureDetector(
                       onTap: () {
-                        _showBottomSheet(context);
+                        showModalBottomSheet(
+                          context: context,
+                          backgroundColor: Colors.transparent,
+                          isScrollControlled: true,
+                          shape: const RoundedRectangleBorder(
+                            borderRadius: BorderRadius.vertical(
+                              top: Radius.circular(30),
+                            ),
+                          ),
+                          builder: (BuildContext context) {
+                            return ProfileModalWidget(onImageSelected: (XFile image) {
+                              setState(() {
+                                _selectedImage = image;
+                              });
+                            });
+                          },
+                        );
                       },
-                      child: Icon(
-                        Icons.camera_alt,
-                        color: Colors.teal,
-                      ),
+                      child: const Icon(Icons.camera_alt, color: Colors.teal,),
                     ),
                   ],
                 ),
-                const SizedBox(
-                  width: 20.0,
-                ),
+                const SizedBox(width: 20.0,),
                 Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     Padding(
                       padding: const EdgeInsets.only(top: 13.0),
-                      child: Text(
-                        MihiAppText.chrisAni,
-                        style: TextStyle(
-                            fontSize: 18.0,
-                            fontWeight: FontWeight.w500,
-                            color: sundayNiqab),
-                      ),
+                      child: Text(MihiAppText.chrisAni, style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.w500, color: sundayNiqab,),),
                     ),
-                    const SizedBox(
-                      height: 10.0,
-                    ),
-                    Text(
-                      MihiAppText.lag,
-                      style: TextStyle(
-                          fontSize: 14.0,
-                          fontWeight: FontWeight.w400,
-                          color: sundayNiqab),
-                    ),
+                    const SizedBox(height: 10.0,),
+                    Text( MihiAppText.lag, style: TextStyle(fontSize: 14.0, fontWeight: FontWeight.w400, color: sundayNiqab,),),
                   ],
                 ),
               ],
             ),
-            const SizedBox(
-              height: 40.0,
-            ),
+            const SizedBox(height: 40.0,),
 
             // Full Name
             TextWidget(textPath2: MihiAppText.fn),
-            const SizedBox(
-              height: 5.0,
-            ),
-            TextField2(
-                hintTextPath: MihiAppText.chrisAni,
-                suffixIconPath: MihiAppAssetsPath.greenTick),
-            const SizedBox(
-              height: 25.0,
-            ),
-
+            const SizedBox(height: 5.0,),
+            TextField2(hintTextPath: MihiAppText.chrisAni, suffixIconPath: MihiAppAssetsPath.greenTick,),
+            const SizedBox(height: 25.0,),
             // Email Address
             TextWidget2(emailPath: MihiAppText.emailAdress),
-            const SizedBox(
-              height: 10.0,
-            ),
-            const SizedBox(
-              height: 2.0,
-            ),
-            TextField2(
-                hintTextPath: MihiAppText.mail,
-                suffixIconPath: MihiAppAssetsPath.edit2),
-            const SizedBox(
-              height: 25.0,
-            ),
-
+            const SizedBox(height: 12.0,),
+            TextField2(hintTextPath: MihiAppText.mail, suffixIconPath: MihiAppAssetsPath.edit2,),
+            const SizedBox(height: 25.0,),
             // Phone Number
             TextWidget2(emailPath: MihiAppText.phoneNumber),
-            const SizedBox(
-              height: 10.0,
-            ),
-            const SizedBox(
-              height: 2.0,
-            ),
-            TextField2(
-                hintTextPath: MihiAppText.number,
-                suffixIconPath: MihiAppAssetsPath.edit2),
-            const SizedBox(
-              height: 25.0,
-            ),
-
+            const SizedBox(height: 12.0,),
+            TextField2(hintTextPath: MihiAppText.number, suffixIconPath: MihiAppAssetsPath.edit2,),
+            const SizedBox(height: 25.0,),
             // Address
             Padding(
               padding: const EdgeInsets.only(right: 265.0),
-              child: Text(
-                MihiAppText.addy,
-                style: TextStyle(
-                    fontSize: 16.0,
-                    fontWeight: FontWeight.w400,
-                    color: mithril),
-              ),
+              child: Text(MihiAppText.addy, style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.w400, color: mithril,),),
             ),
-            const SizedBox(
-              height: 10.0,
-            ),
-            const SizedBox(
-              height: 2.0,
-            ),
-            TextField2(
-                hintTextPath: MihiAppText.lekki,
-                suffixIconPath: MihiAppAssetsPath.edit2),
+            const SizedBox(height: 12.0,),
+            TextField2(hintTextPath: MihiAppText.lekki, suffixIconPath: MihiAppAssetsPath.edit2,),
+            const SizedBox(height: 10.0),
           ],
         ),
       ),
+    );
+  }
+}
+
+class ProfileModalWidget extends StatelessWidget {
+  final void Function(XFile) onImageSelected;
+
+  const ProfileModalWidget({
+    super.key,
+    required this.onImageSelected,
+  });
+
+  Future<void> _pickImage(ImageSource source, BuildContext context) async {
+    final ImagePicker picker = ImagePicker();
+    final XFile? image = await picker.pickImage(source: source);
+    if (image != null) {
+      onImageSelected(image);
+      Navigator.pop(context); // Close the bottom sheet after selecting an image
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 200,
+      decoration: BoxDecoration(
+        color: whiteText,
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(32),
+          topRight: Radius.circular(32),
+        ),
+      ),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          //************* Upload from media ******/ 
+          GestureDetector(
+            onTap: () {
+              _pickImage(ImageSource.gallery, context);
+            },
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 30.0, left: 20.0),
+                  child: Container(
+                    height: 40,
+                    width: 40,
+                    decoration: BoxDecoration(
+                      color: Brilliant,
+                      borderRadius: BorderRadius.circular(16),
+                      image: DecorationImage(image: AssetImage(MihiAppAssetsPath.upload),),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 20.0, top: 30.0),
+                  child: Text(MihiAppText.upload, style: TextStyle(color: blackText, fontWeight: FontWeight.w400, fontSize: 20,),),
+                ),
+              ],
+            ),
+          ),
+          //******** Take a live photo ********/
+          GestureDetector(
+            onTap: () {
+              _pickImage(ImageSource.camera, context);
+            },
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 40.0, left: 20.0),
+                  child: Container(
+                    height: 40,
+                    width: 40,
+                    decoration: BoxDecoration(
+                      color: Brilliant,
+                      borderRadius: BorderRadius.circular(16),
+                      image: DecorationImage(image: AssetImage(MihiAppAssetsPath.camera),),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 20.0, top: 40.0),
+                  child: Text(MihiAppText.photo, style: TextStyle(color: blackText, fontWeight: FontWeight.w400, fontSize: 20,),),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class TextWidget2 extends StatelessWidget {
+  const TextWidget2({
+    super.key,
+    required this.emailPath,
+  });
+
+  final String emailPath;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(right: 225.0),
+      child: Text(emailPath, style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.w400, color: mithril),),
+    );
+  }
+}
+
+class TextWidget extends StatelessWidget {
+  const TextWidget({
+    super.key,
+    required this.textPath2,
+  });
+
+  final String textPath2;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(right: 255.0),
+      child: Text(textPath2, style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.w400, color: mithril),),
     );
   }
 }
@@ -311,55 +280,14 @@ class TextField2 extends StatelessWidget {
             contentPadding: EdgeInsets.all(20),
             isCollapsed: true,
             hintText: hintTextPath,
-            hintStyle: TextStyle(
-                color: mithril, fontSize: 16.0, fontWeight: FontWeight.w400),
+            hintStyle: TextStyle(color: mithril, fontSize: 16.0, fontWeight: FontWeight.w400),
             filled: true,
             fillColor: bleachedSilk,
-            enabledBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(8),
-                borderSide: BorderSide(color: mithril)),
+            enabledBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(8), borderSide: BorderSide(color: mithril),),
             suffixIcon: Image.asset(suffixIconPath),
           ),
         ),
       ),
-    );
-  }
-}
-
-class TextWidget2 extends StatelessWidget {
-  const TextWidget2({
-    super.key,
-    required this.emailPath,
-  });
-
-  final String emailPath;
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(right: 225.0),
-      child: Text(emailPath,
-          style: TextStyle(
-              fontSize: 16.0, fontWeight: FontWeight.w400, color: mithril)),
-    );
-  }
-}
-
-class TextWidget extends StatelessWidget {
-  const TextWidget({
-    super.key,
-    required this.textPath2,
-  });
-
-  final String textPath2;
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(right: 255.0),
-      child: Text(textPath2,
-          style: TextStyle(
-              fontSize: 16.0, fontWeight: FontWeight.w400, color: mithril)),
     );
   }
 }
